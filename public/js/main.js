42d76eb631a9a0f603c081011f2a6bc7802bd6c6
Tarun Boddupalli <tarunbod@gmail.com>
2017-03-25 13:04:23 -0400

    ok

--- public/js/main.js
+++ public/js/main.js
@@ -2,10 +2,13 @@ var socket = io();
 var msgElem = document.getElementById("msg");
 
 window.ondevicemotion = function(event) {
-    
-	var accelerationX = event.accelerationIncludingGravity.x;
-	var accelerationY = event.accelerationIncludingGravity.y;
-	var accelerationZ = event.accelerationIncludingGravity.z;
+
+	var accelerationX = event.acceleration.x;
+    accelerationX = Math.floor(accelerationX * 100) / 100;
+	var accelerationY = event.acceleration.y;
+    accelerationY = Math.floor(accelerationY * 100) / 100;
+	var accelerationZ = event.acceleration.z;
+    accelerationZ = Math.floor(accelerationZ * 100) / 100;
 
     socket.emit('motion', {
         acc: {
